01e6a7d06250a4078bfe6001741e6d83ce3d2330
Nícolas Deves <nicolas.deves@universo.univates.br>
2024-10-08 23:46:42 -0300

    Feat adicionar rotas de get meals

--- src/routes/meal.ts
+++ src/routes/meal.ts
@@ -3,6 +3,24 @@ import { db } from '../database'
 import { z } from 'zod'
 
 export async function mealRoutes(app: FastifyInstance) {
+  app.get('/', async () => {
+    const meals = await db('meal').select()
+
+    return { meals }
+  })
+
+  app.get('/:id', async (request) => {
+    const getMealParamsSchema = z.object({
+      id: z.string(),
+    })
+
+    const { id } = getMealParamsSchema.parse(request.params) //pega o que está na rota
+
+    const meal = await db('meal').where('id', id).first()
+
+    return { meal }
+  })
+
   app.post('/', async (request, reply) => {
     const createMealBodySchema = z.object({
       name: z.string(),
